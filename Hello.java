01a1f755cda33a056d12ddee84e1fc76e08e0005
Maciej Zubala <maciej.zubala@gmail.com>
2017-12-16 13:39:07 +0100

    commit from intelli

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello VVVVVVV");
 		System.out.println("Hello ZZZZZZZ");
+		System.out.println("Ale jaja");
 	}
 
 	public void foo() {}
