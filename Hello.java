41b98de6d9a7f792c41d7ea16f7e0bbdea79ebed
Maciej Zubala <maciej.zubala@gmail.com>
2017-12-16 13:04:58 +0100

    nowy ficzer

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello VVVVVVV");
-		System.out.println("Hello YYYYY");
+		System.out.println("Hello ZZZZZZZ");
 	}
 
 	public void foo() {}
