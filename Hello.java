d2af8ad08db247adbf7ae6a8bcf306708a5b9b0f
Maciej Zubala <maciej.zubala@gmail.com>
2017-12-16 13:52:43 +0100

    a to moje lepsze zmiany

--- Hello.java
+++ Hello.java
@@ -1,11 +1,16 @@
 class Hello {
-	
+
+	public Hello() {}
+
 	public static void main(String[] args) {
 		System.out.println("Hello VVVVVVV");
 		System.out.println("Hello ZZZZZZZ");
 		System.out.println("Ale jaja");
 	}
 
-	public void foo() {}
+	public void foo() {
+		System.out.println("Moje inne zmiany");
+		System.out.print("Ja ja cie");
+	}
 
 }
